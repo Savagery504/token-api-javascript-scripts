f1759fd4bf1948367338ded7e3cbc0f0b30dae96
Savagery504 <durrelljeanpierre@gmail.com>
2025-03-30 13:01:03 -0500

    Update fetch-script.js

--- fetch-script.js
+++ fetch-script.js
@@ -2,10 +2,10 @@
 import fetch from 'node-fetch';
 
 // Replace with your Alchemy API key:
-const apiKey = "demo";
+const apiKey = "APrivateKey1zkp7sGeyGFSj8MF7RRm6qL1RkYckNce3BTEwSyCFpCmhA6C;
 const fetchURL = `https://eth-mainnet.g.alchemy.com/v2/${apiKey}`;
 
-// Replace with the wallet address you want to query:
+// Replace with the wallet address you want to query:0xa35098d15d7096956f691f9363d7804ebfaec158dff09fa39f6d7e80d601727e
 const ownerAddr = "0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be";
 // Replace with the token contract address you want to query:
 const tokenAddr = "0x607f4c5bb672230e8672085532f7e901544a7375";
@@ -17,9 +17,9 @@ var raw = JSON.stringify({
     "Content-Type": "application/json"
   },
   "params": [
-    `${ownerAddr}`,
+    `${ownerAddr}`,0xa35098d15d7096956f691f9363d7804ebfaec158dff09fa39f6d7e80d601727e
     [
-      `${tokenAddr}`,
+      `${tokenAddr}`,0x607f4c5bb672230e8672085532f7e901544a7375
     ]
   ],
   "id": 42
